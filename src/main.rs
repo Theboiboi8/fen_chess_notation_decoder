e8810397a64cfab7a1d4f95a6d21b8d16efd3665
Theboiboi8 <kamentanchev@gmail.com>
2024-04-23 18:47:53 +0300

    Increase readability in test cases

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use std::fs;
 mod parser;
 
 fn main() {
-    let starting_pos_file =
+    let starting_position_file =
         fs::read_to_string("fen_starting_position.txt")
             .unwrap_or_default();
 
-    let starting_pos_file_stripped =
+    let starting_position_file_stripped =
         fs::read_to_string("fen_starting_position_stripped.txt")
             .unwrap_or_default();
 
@@ -15,21 +15,28 @@ fn main() {
         fs::read_to_string("fen_variety_test.txt")
             .unwrap_or_default();
 
-    let starting_position_file = parser::Fen::from_string(starting_pos_file_stripped.as_str());
-    let variety_test_file = parser::Fen::from_string(variety_test_file.as_str());
+    let starting_position = parser::Fen::from_string(starting_position_file_stripped.as_str());
+    let variety_test = parser::Fen::from_string(variety_test_file.as_str());
 
-    println!("{starting_pos_file}");
-    println!();
-    println!("{starting_pos_file_stripped}");
-    println!();
-    println!("{}", parser::Fen::default());
-    println!();
+    println!("--- WIKI EXAMPLE: STARTING POSITION ---");
     println!("{starting_position_file}");
-    println!();
-    println!("{variety_test_file}");
-    println!();
+    
+    println!("--- WIKI EXAMPLE: STARTING POSITION STRIPPED ---");
+    println!("{starting_position_file_stripped}");
+    
+    println!("--- FEN: DEFAULT ---");
+    println!("{}", parser::Fen::default());
+    
+    println!("--- FEN: STARTING POSITION ---");
+    println!("{starting_position}");
+    
+    println!("--- FEN: VARIETY TEST ---");
+    println!("{variety_test}");
+    
+    println!("--- ROW: EMPTY ---");
     println!("{}", parser::Row::empty());
-    println!();
+    
+    println!("--- ROW: LINE-BY-LINE ---");
     for (i, row) in parser::Fen::default().rows.iter().enumerate() {
         println!("{row}, {i}");
     }
